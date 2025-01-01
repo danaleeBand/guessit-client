3da39ae2c6ffc5df7b5815ce352d32d9d0d1779d
danalee <eleeje97@gmail.com>
2025-01-02 02:18:30 +0900

    :heavy_plus_sign: shadcn 컴포넌트 라이브러리 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
