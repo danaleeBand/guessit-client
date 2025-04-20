49343a2a04f838d5692f1c930c36515e41d301b0
yujch716 <70509403+yujch716@users.noreply.github.com>
2025-04-20 15:22:38 +0900

    :sparkles: room detail

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # guessit-client
 guessit 클라이언트 (React PWA)
+
+
+### shadcn
+https://ui.shadcn.com/
---
+++ src/pages/Room.tsx
@@ -0,0 +1,129 @@
+import { SimpleGrid } from '@chakra-ui/react'
+import { ResizablePanel, ResizablePanelGroup } from '../components/ui/resizable'
+import { Settings } from 'lucide-react'
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from '../components/ui/input-otp'
+import { Separator } from '../components/ui/separator'
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from '../components/ui/avatar.tsx'
+
+const defaultRoom = {
+  title: '방 제목',
+  players: [
+    {
+      id: 1,
+      name: '유저1',
+      profileUrl: 'https://i.postimg.cc/J76717dq/2025-02-07-9-11-10.png',
+      score: 2,
+    },
+    {
+      id: 2,
+      name: '유저2',
+      profileUrl: 'https://i.postimg.cc/J76717dq/2025-02-07-9-11-10.png',
+      score: 3,
+    },
+    {
+      id: 3,
+      name: '유저3',
+      profileUrl: 'https://i.postimg.cc/J76717dq/2025-02-07-9-11-10.png',
+      score: 10,
+    },
+    {
+      id: 4,
+      name: '유저4',
+      profileUrl: 'https://i.postimg.cc/J76717dq/2025-02-07-9-11-10.png',
+      score: 0,
+    },
+    {
+      id: 5,
+      name: '유저5',
+      profileUrl: 'https://i.postimg.cc/J76717dq/2025-02-07-9-11-10.png',
+      score: 0,
+    },
+  ],
+  quizAnswerLength: 4,
+  quizHints: ['태양', '노란색', '햄스터', '씨앗'],
+}
+
+export default function Room() {
+  return (
+    <div>
+      <div className="mt-10 flex flex-col items-center text-center">
+        <SimpleGrid columns={1} gap="4">
+          <div className="mt-10 flex w-full flex-row justify-between items-center">
+            <h1 className="text-3xl font-semibold text-gray-800">
+              {defaultRoom.title}
+            </h1>
+            <Settings />
+          </div>
+          <div className="flex justify-end">
+            <ResizablePanelGroup
+              direction="vertical"
+              className="min-h-[50px] max-w-min rounded-lg border"
+            >
+              <ResizablePanel defaultSize={75}>
+                <div className="flex h-full items-center justify-center p-6">
+                  <span className="font-semibold">1/10</span>
+                </div>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </div>
+
+          <ResizablePanelGroup
+            direction="vertical"
+            className="min-h-[300px] max-w-md rounded-lg md:min-w-[600px]"
+          >
+            <ResizablePanel defaultSize={75}>
+              <div className="flex h-full items-center justify-center p-6 bg-gray-100">
+                <div className="grid grid-cols-2 gap-4">
+                  {defaultRoom.quizHints.map((hint, index) => (
+                    <span key={index} className="font-semibold">
+                      {hint}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+
+          <div className="w-full flex justify-center items-center my-10">
+            <InputOTP maxLength={defaultRoom.quizAnswerLength}>
+              <InputOTPGroup>
+                {Array.from({ length: defaultRoom.quizAnswerLength }).map(
+                  (_, index) => (
+                    <InputOTPSlot key={index} index={index} />
+                  ),
+                )}
+              </InputOTPGroup>
+            </InputOTP>
+          </div>
+          <Separator />
+        </SimpleGrid>
+
+        <div className="mt-8 flex justify-center gap-8 flex-wrap">
+          {defaultRoom.players.map((player) => (
+            <div
+              key={player.id}
+              className="flex flex-col items-center space-y-2"
+            >
+              <Avatar className="bg-gray-200 text-black">
+                <AvatarFallback>{player.id}</AvatarFallback>
+                <AvatarImage src={player.profileUrl} />
+              </Avatar>
+              <div className="text-sm">{player.name}</div>
+              <div className="rounded-md border px-2 py-1 text-xs">
+                {player.score}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
